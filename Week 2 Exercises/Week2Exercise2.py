068648c6a298086ff2186a99873e9882d1b8ccc7
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2020-07-18 22:29:29 +0100

    Added exercise description

--- Week 2 Exercises/Week2Exercise2.py
+++ Week 2 Exercises/Week2Exercise2.py
@@ -1,3 +1,15 @@
+"""
+2. Create a list of five IP addresses.
+
+Use the .append() method to add an IP address onto the end of the list.
+Use the .extend() method to add two more IP addresses to the end of the list.
+Use list concatenation to add two more IP addresses to the end of the list.
+Print out the entire list of ip addresses. Print out the first IP address in the list.
+Print out the last IP address in the list.
+Using the .pop() method to remove the first IP address in the list and the last IP address in the list.
+Update the new first IP address in the list to be '2.2.2.2'. Print out the new first IP address in the list.
+"""
+
 # List of 5 IP addresses
 ip_add = ['192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.4', '192.168.1.5']
 # Append one more IP address to end using append method
