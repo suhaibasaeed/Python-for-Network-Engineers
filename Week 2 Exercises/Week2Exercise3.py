ca82d987ac0f014b24726535973e4b74f875c4d8
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2020-07-19 13:57:27 +0100

    Added exercise description

--- Week 2 Exercises/Week2Exercise3.py
+++ Week 2 Exercises/Week2Exercise3.py
@@ -1,3 +1,20 @@
+"""
+3. Read in the "show_arp.txt" file using the readlines() method. Use a list slice to remove the header line.
+
+Use pretty print to print out the resulting list to the screen, syntax is:
+from pprint import pprint
+pprint(some_var)
+
+Use the list .sort() method to sort the list based on IP addresses.
+
+Create a new list slice that is only the first three ARP entries.
+
+Use the .join() method to join these first three ARP entries back together.
+Should be a single string using the newline character ('\n') as the separator.
+
+Write this string containing the three ARP entries out to a file named "arp_entries.txt".
+"""
+
 from pprint import pprint
 # Open the file
 show_arp = open("show_arp.txt")
